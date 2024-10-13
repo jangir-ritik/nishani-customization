b27845bd4d34d4f8fbf4ca809513af8e5551d74c
Ritik <jangir.ritik06@gmail.com>
2024-10-13 05:30:52 +0530

    fix: codebase - build issues

--- src/app/components/common/ProductImage.tsx
+++ src/app/components/common/ProductImage.tsx
@@ -4,17 +4,8 @@ import useProductStore from "@/app/store/store";
 import productImage from "@/public/pngs/view-container.png";
 
 const ProductImage: React.FC = () => {
-    const selectedChainPart = useProductStore(state => state.selectedChainPart);
-    const selectedChainPartLabel = useProductStore(state => state.chainParts[selectedChainPart].label);
-    // const leftChainModels = useProductStore(state => state.leftChainModels);
 
     const getContentToDisplay = () => {
-        if (selectedChainPartLabel === 'Left Chain') {
-            return {
-                type: 'text',
-                content: `Todo Add Model ${selectedChainPart + 1}`
-            };
-        }
         return {
             type: 'image',
             src: productImage,
@@ -34,7 +25,7 @@ const ProductImage: React.FC = () => {
                 />
             ) : (
                 <div className="product-text-content">
-                    {content.content}
+                    test
                 </div>
             )}
         </div>
--- src/app/components/common/ProductVariantModel.tsx
+++ src/app/components/common/ProductVariantModel.tsx
@@ -1,39 +1,49 @@
+// import React from 'react'
+// import Image from 'next/image'
+// import useProductStore from '@/app/store/store'
+
+// function ProductVariantModel() {
+//     const selectedChainPart = useProductStore(state => state.selectedChainPart)
+//     const selectedChainPartModel = useProductStore(state => state.selectedChainPartModel)
+//     const setSelectedChainPartModel = useProductStore(state => state.setSelectedChainPartModel)
+//     const chainPartModels = useProductStore(state => state.chainPartModels)
+//     const chainParts = useProductStore(state => state.chainParts)
+
+//     const handleChainPartModelClick = (index: number) => {
+//         setSelectedChainPartModel(index === selectedChainPartModel ? null : index)
+//     }
+
+//     return (
+//         <div className="product-variant-model-container">
+//             {chainParts[selectedChainPart]?.label === 'Left Chain' && (
+//                 <div className="product-variant-model-left-chain">
+//                     {chainPartModels.map((model, index) => (
+//                         <div 
+//                             key={index} 
+//                             className={`left-chain-model ${selectedChainPartModel === index ? 'selected' : 'default'}`}
+//                             onClick={() => handleChainPartModelClick(index)}
+//                         >
+//                             <Image src={model} alt={`left-chain-model-${index + 1}`} width={100} height={100} />
+//                         </div>
+//                     ))}
+//                 </div>
+//             )}
+//             {chainParts[selectedChainPart]?.label === 'Front Lock' && (
+//                 <p>This is the front lock of the product.</p>
+//             )}
+//             {/* Add more conditions for other chain types */}
+//         </div>
+//     )
+// }
+
+// export default ProductVariantModel
+
 import React from 'react'
-import Image from 'next/image'
-import useProductStore from '@/app/store/store'
 
 function ProductVariantModel() {
-    const selectedChainPart = useProductStore(state => state.selectedChainPart)
-    const selectedChainPartModel = useProductStore(state => state.selectedChainPartModel)
-    const setSelectedChainPartModel = useProductStore(state => state.setSelectedChainPartModel)
-    const chainPartModels = useProductStore(state => state.chainPartModels)
-    const chainParts = useProductStore(state => state.chainParts)
-
-    const handleChainPartModelClick = (index: number) => {
-        setSelectedChainPartModel(index === selectedChainPartModel ? null : index)
-    }
-
-    return (
-        <div className="product-variant-model-container">
-            {chainParts[selectedChainPart]?.label === 'Left Chain' && (
-                <div className="product-variant-model-left-chain">
-                    {chainPartModels.map((model, index) => (
-                        <div 
-                            key={index} 
-                            className={`left-chain-model ${selectedChainPartModel === index ? 'selected' : 'default'}`}
-                            onClick={() => handleChainPartModelClick(index)}
-                        >
-                            <Image src={model} alt={`left-chain-model-${index + 1}`} width={100} height={100} />
-                        </div>
-                    ))}
-                </div>
-            )}
-            {chainParts[selectedChainPart]?.label === 'Front Lock' && (
-                <p>This is the front lock of the product.</p>
-            )}
-            {/* Add more conditions for other chain types */}
-        </div>
-    )
+  return (
+    <div>ProductVariantModel</div>
+  )
 }
 
 export default ProductVariantModel
